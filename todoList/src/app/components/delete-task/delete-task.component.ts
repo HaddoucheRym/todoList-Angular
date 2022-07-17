9ca20a9505602344da75d4bb3bb90cb473f52f54
Haddouche Rym <rymmihoub@gmail.com>
2022-07-17 23:09:40 +0200

    utiliser @Input pour apporter la variable listTask du todoPage et creer une methode delete dans deleteTask.ts et creer un bouton dans deleteTask.html et le faire appeler dans display.html

--- todoList/src/app/components/delete-task/delete-task.component.ts
+++ todoList/src/app/components/delete-task/delete-task.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-delete-task',
@@ -6,10 +6,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./delete-task.component.css']
 })
 export class DeleteTaskComponent implements OnInit {
-
+  @Input()
+  tache : any;
   constructor() { }
 
   ngOnInit(): void {
   }
-
+delete(index:number):void{
+this.tache.splice(1,index);
+}
 }
